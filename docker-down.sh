e3ee2209d15e3df19f0c45084202b80a95793a0e
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2025-02-22 21:20:53 +0700

    MTB-55: finish dockerize

--- docker-down.sh
+++ docker-down.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml --env-file .env down
+
+COMPOSE_FILE="docker-compose.yml"
+DOCKER_CMD="docker-compose"
+
+for arg in "$@"
+do
+    if [ "$arg" == "--debug" ]; then
+        COMPOSE_FILE="docker-compose.debug.yml"
+    elif [ "$arg" == "--core" ]; then
+        DOCKER_CMD="docker compose"
+    fi
+done
+
+$DOCKER_CMD -f $COMPOSE_FILE --env-file .env down
--- docker-up.sh
+++ docker-up.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-touch backend/.env.local
-./docker-prepare-build-fe.sh
 
-# Check if the --build parameter is passed
-if [[ "$1" == "--build" ]]; then
-    docker-compose -f docker-compose.yml --env-file .env up -d --build
-else
-    docker-compose -f docker-compose.yml --env-file .env up -d
-fi
+# Determine which docker-compose file to use
+COMPOSE_FILE="docker-compose.yml"
+DOCKER_CMD="docker-compose"
+BUILD_FLAG=""
+
+for arg in "$@"
+do
+    if [ "$arg" == "--debug" ]; then
+        COMPOSE_FILE="docker-compose.debug.yml"
+    elif [ "$arg" == "--build" ]; then
+        BUILD_FLAG="--build"
+    elif [ "$arg" == "--core" ]; then
+        DOCKER_CMD="docker compose"
+    fi
+done
+
+# docker-compose -f docker-compose.yml --env-file .env up -d
+$DOCKER_CMD -f $COMPOSE_FILE --env-file .env up -d $BUILD_FLAG
