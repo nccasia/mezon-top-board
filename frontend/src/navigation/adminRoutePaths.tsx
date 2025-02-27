143b830512a26d5e28f62f600a12a62ca28f4126
Quan Nguyen Ton Minh <minhquan02052003@gmail.com>
2025-02-27 17:18:40 +0700

    feat: MTB-57-List-All-Apps

--- frontend/src/navigation/adminRoutePaths.tsx
+++ frontend/src/navigation/adminRoutePaths.tsx
@@ -1,33 +1,27 @@
 // src/routes/adminRoutePaths.ts
 
-import Dashboard from "@app/pages/AdminPage/Dashboard";
+import { AppstoreAddOutlined, SettingOutlined, UserOutlined } from "@ant-design/icons";
+import MezonApps from "@app/pages/AdminPage/MezonApps";
 import { RoutePath } from "@app/types/RoutePath.types";
 
 
 export const adminRoutePaths: RoutePath[] = [
   {
-    path: "",
-    element: <Dashboard />, // Default page when accessing /admin
-    label: "Dashboard",
+    path: "apps",
+    element: <MezonApps />, // Default page when accessing /admin
+    label: "Apps",
+    icon: <AppstoreAddOutlined/>
   },
   {
     path: "users",
     element: <h1>Users</h1>,
     label: "Users",
-  },
-  {
-    path: "orders",
-    element: <h1>Orders</h1>,
-    label: "Orders",
-  },
-  {
-    path: "products",
-    element: <h1>Products</h1>,
-    label: "Products",
+    icon: <UserOutlined/>
   },
   {
     path: "settings",
     element: <h1>Settings</h1>,
     label: "Settings",
+    icon: <SettingOutlined/>
   },
 ];
