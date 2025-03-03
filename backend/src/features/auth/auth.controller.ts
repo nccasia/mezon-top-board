57844e0f75d87455905c68a6333c2908e99a8a34
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2025-03-03 21:44:30 +0700

    update generate state code from BE side

--- backend/src/features/auth/auth.controller.ts
+++ backend/src/features/auth/auth.controller.ts
@@ -21,12 +21,11 @@ export class AuthController {
 
   @Public()
   @Get("redirect")
-  @ApiQuery({ name: "state", required: true })
-  async redirect(@Req() req, @Res() res, @Query('state') state: any) {
+  async redirect(@Req() req, @Res() res) {
     const OAUTH2_API_URL = config().OAUTH2_API_URL || ''
     const CLIENT_ID = config().OAUTH2_CLIENT_ID
     const REDIRECT_URI = config().OAUTH2_REDIRECT_URI || ''
-    const STATE = state || Math.random().toString(36).substring(2, 15)
+    const STATE = Math.random().toString(36).substring(2, 15)
 
     const authUrl = `${OAUTH2_API_URL}/oauth2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=openid+offline&state=${STATE}`
     return res.redirect(authUrl);
--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,10 +1,7 @@
 export const redirectToOAuth = () => {
   const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || ''
-  const STATE = Math.random().toString(36).substring(2, 15)
 
-  sessionStorage.setItem('oauth_state', STATE)
-
-  const authUrl =`${BACKEND_URL}/api/auth/redirect?state=${STATE}`
+  const authUrl =`${BACKEND_URL}/api/auth/redirect`
 
   window.location.href = authUrl
 }
