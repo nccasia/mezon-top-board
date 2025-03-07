5f41199df80ce9be44c823b9678f55e516e6b99f
toannt <thaitoan3039015@gmail.com>
2025-03-07 14:53:53 +0700

    mtb-63: crud tag

--- backend/src/features/tag/tag.controller.ts
+++ backend/src/features/tag/tag.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+import { RequestWithId } from "@domain/common/dtos/request.dto";
+
+import { Public } from "@libs/decorator/authorization.decorator";
 import { Logger } from "@libs/logger";
 
+import { CreateTagRequest, UpdateTagRequest } from "./dtos/request";
 import { TagResponse } from "./dtos/response";
 import { TagService } from "./tag.service";
-import { Public } from "@libs/decorator/authorization.decorator";
+
 
 @Controller("tag")
 @ApiTags("Tag")
@@ -22,5 +26,41 @@ export class TagController {
   @ApiResponse({ type: TagResponse })
   async getTags() {
     return this.tagService.getTagAll();
+  }
+
+  @Public()
+  @Post()
+  @ApiBody({ type: CreateTagRequest })
+  createTag(@Body() body: CreateTagRequest) {
+    try {
+      return this.tagService.createTag(body);
+    } catch (error) {
+      this.logger.error("An error occured", error);
+      throw error;
+    }
+  }
+
+  @Public()
+  @Put()
+  @ApiBody({ type: UpdateTagRequest })
+  updateTag(@Body() body: UpdateTagRequest) {
+    try {
+      return this.tagService.updateTag(body);
+    } catch (error) {
+      this.logger.error("An error occured", error);
+      throw error;
+    }
+  }
+
+  @Public()
+  @Delete()
+  @ApiBody({ type: RequestWithId })
+  deleteTag(@Body() body: RequestWithId) {
+    try {
+      return this.tagService.deleteTag(body);
+    } catch (error) {
+      this.logger.error("An error occured", error);
+      throw error;
+    }
   }
 }
