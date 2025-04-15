7f645fd941e70b5c941ec88d18f740b772fc9abd
Quan Nguyen Ton Minh <minhquan02052003@gmail.com>
2025-04-15 15:36:29 +0700

    MTB-177: unable to save

--- frontend/src/pages/AdminPage/AdminMezonApp/EditAppModal.tsx
+++ frontend/src/pages/AdminPage/AdminMezonApp/EditAppModal.tsx
@@ -49,10 +49,23 @@ const EditModal = ({
     >
       <div className="max-h-[60vh] overflow-y-auto">
         <Form form={form} layout="horizontal" labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} className="max-w-full">
-          <Form.Item name="name" label="App Name" rules={[{ required: true, message: "App Name is required" }]}>
+          <Form.Item name="name" label="App Name" rules={[
+            { required: true, message: "App Name is required" },
+            { min: 3, message: "App Name must be at least 3 characters" },
+            { max: 128, message: "App Name must not exceed 128 characters" },
+          ]}>
             <Input />
           </Form.Item>
-          <Form.Item name="headline" label="Headline">
+          <Form.Item name="headline" label="Headline" rules={[
+            () => ({
+              // antd's custom validator
+              validator(_, value) {
+                if (!value || value.length < 50) return Promise.reject("Headline must be at least 50 characters");
+                if (value.length > 510) return Promise.reject("Headline must not exceed 510 characters");
+                return Promise.resolve();
+              },
+            }),
+          ]}>
             <Input />
           </Form.Item>
           <Form.Item name="description" label="Description">
