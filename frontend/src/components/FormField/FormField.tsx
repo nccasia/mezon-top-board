f11941508f43df5bbf7a5e36bc7197490c21c851
tranuyn <127413543+tranuyn@users.noreply.github.com>
2025-04-21 17:13:20 +0700

    Broken layout due to link in headline and search box

--- frontend/src/components/FormField/FormField.tsx
+++ frontend/src/components/FormField/FormField.tsx
@@ -10,6 +10,8 @@ interface FormFieldProps {
 
 function FormField({ label, description = '', children, errorText, customClass }: FormFieldProps) {
   return (
+    <div className={`flex items-start pt-10 gap-6 ${customClass}`}>
+      <div className='flex flex-col w-1/4'>
         <p className='text-[18px] '>{label}</p>
         <p className='text-[14px] text-gray-500'>{description}</p>
       </div>
