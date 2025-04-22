080eb6c6a24055b36937758016552084e0a4fc35
Tran Huynh Nha Uyen <127413543+tranuyn@users.noreply.github.com>
2025-04-22 10:09:06 +0700

    MTB_170_create useAdminCheck for AdminLayout (#121)

---
+++ frontend/src/hook/useAdminCheck.ts
@@ -0,0 +1,26 @@
+import { RootState } from '@app/store'
+import { IUserStore } from '@app/store/user'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+const useAdminCheck = () => {
+  const navigate = useNavigate()
+  const { userInfo } = useSelector<RootState, IUserStore>((s) => s.user)
+
+  const checkAdmin = () => {
+    if (userInfo.role !== 'ADMIN') {
+      toast.warning('You do not have permission to access this page.')
+      navigate('/')
+      return false
+    }
+
+    return true
+  }
+
+  return {
+    checkAdmin
+  }
+}
+
+export default useAdminCheck
