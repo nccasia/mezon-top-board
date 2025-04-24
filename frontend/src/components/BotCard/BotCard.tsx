924dfc1371b125bffa7b4d64b00fd888cc649519
tranuyn <127413543+tranuyn@users.noreply.github.com>
2025-04-24 12:01:01 +0700

    split existing button into OwnerActions component

--- frontend/src/components/BotCard/BotCard.tsx
+++ frontend/src/components/BotCard/BotCard.tsx
@@ -10,13 +10,10 @@ import { getUrlImage, safeConcatUrl, uuidToNumber } from '@app/utils/stringHelpe
 import { Popover, Tag } from 'antd'
 import { useNavigate } from 'react-router-dom'
 import ShareButton from './components/ShareButton'
-import { Dropdown, MenuProps, Modal } from 'antd'
-import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
-import { useMezonAppControllerDeleteMezonAppMutation } from '@app/services/api/mezonApp/mezonApp'
-import { toast } from 'react-toastify'
 import { useSelector } from 'react-redux'
 import { RootState } from '@app/store'
 import { IUserStore } from '@app/store/user'
+import OwnerActions from '../OwnerActions/OwnerActions'
 
 function BotCard({ readonly = false, data, canNavigateOnClick = true }: IBotCardProps) {
   const { userInfo } = useSelector<RootState, IUserStore>((s) => s.user)
@@ -28,63 +25,13 @@ function BotCard({ readonly = false, data, canNavigateOnClick = true }: IBotCard
   const handleShare = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation()
   }
-
+  console.log(data?.owner?.id === userInfo?.id)
   const imgUrl = data?.featuredImage ? getUrlImage(data.featuredImage) : avatarBotDefault
   // Share to social media
   const shareUrl = process.env.REACT_APP_SHARE_URL || 'https://top.mezon.ai/bot/'
   const title = data?.name || 'Check out this app!'
-  const handleMenuClick: MenuProps['onClick'] = (e) => {
-    e.domEvent.stopPropagation()
-  }
-  const [deleteBot] = useMezonAppControllerDeleteMezonAppMutation()
-  const { confirm } = Modal
 
-  const handleDeleteBot = (botId: string) => {
-    confirm({
-      title: 'Are you sure you want to delete this bot?',
-      icon: <ExclamationCircleOutlined />,
-      content: 'This action cannot be undone.',
-      okText: 'Yes, delete it',
-      okType: 'danger',
-      cancelText: 'Cancel',
-      onOk: async () => {
-        try {
-          await deleteBot({ requestWithId: { id: botId } }).unwrap()
-          toast.success('Bot deleted successfully.')
-        } catch (error) {
-          toast.error('Failed to delete bot.')
-        }
-      }
-    })
-  }
-  const items: MenuProps['items'] = [
-    {
-      label: 'Edit',
-      key: '1',
-      icon: <EditOutlined />,
-      onClick: () => {
-        navigate(`/new-bot/${data?.id}`)
-      }
-    },
-    {
-      label: 'Delete',
-      key: '2',
-      danger: true,
-      icon: <DeleteOutlined />,
-      onClick: () => {
-        if (!data?.id) {
-          toast.error('Invalid bot ID.')
-          return
-        }
-        handleDeleteBot(data?.id)
-      }
-    }
-  ]
-
-  const menuProps = {
-    items,
-    onClick: handleMenuClick
-  }
+  
   return (
     <div
       className='shadow-md pb-8 pt-8 px-8 border border-gray-300 relative rounded-xl cursor-pointer'
@@ -127,6 +74,9 @@ function BotCard({ readonly = false, data, canNavigateOnClick = true }: IBotCard
             ))}
           </div>
           <div className='sm:absolute sm:top-2 sm:right-2 flex gap-3'>
+            {data?.owner?.id === userInfo?.id && (
+              <OwnerActions data={data} isBotCard={true} />
+            )}
             <Button variant='solid' color='secondary' size='large' onClick={handleInvite}>
               Invite
             </Button>
@@ -153,37 +103,6 @@ function BotCard({ readonly = false, data, canNavigateOnClick = true }: IBotCard
             {data?.headline}
           </div>
         </div>
-      </div>
-      <div className='absolute top-2 right-2 flex gap-3'>
-        {data?.owner?.id === userInfo?.id && (
-          <Dropdown.Button
-            style={{ display: 'contents' }}
-            size='large'
-            buttonsRender={([leftBtn, rightBtn]) => [
-              null,
-              <span onClick={(e) => e.stopPropagation()}>{rightBtn}</span>
-            ]}
-            trigger={['click']}
-            menu={menuProps}
-          />
-        )}
-        <Button variant='solid' color='secondary' size='large' onClick={handleInvite}>
-          Invite
-        </Button>
-        <Popover
-          content={
-            <ShareButton
-              text={`Check out ${title} Mezon Bot on top.nccsoft.vn, the #1 Mezon Bot and Mezon App List!`}
-              url={safeConcatUrl(shareUrl, data?.id || '')}
-            />
-          }
-          trigger='click'
-          placement='bottomRight'
-          arrow={false}
-          overlayInnerStyle={{ marginTop: '8px', minWidth: '200px', maxWidth: '300px' }}
-        >
-          <Button size='large' color='default' icon={<UploadOutlined />} onClick={handleShare} />
-        </Popover>
       </div>
     </div>
   )
