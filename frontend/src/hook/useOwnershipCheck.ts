b8c4cf9172820f8f48f855c2309c3f68a96ef931
Quan Nguyen Ton Minh <minhquan02052003@gmail.com>
2025-04-29 09:39:04 +0700

    MTB-219-error-logout-when-view-bot

--- frontend/src/hook/useOwnershipCheck.ts
+++ frontend/src/hook/useOwnershipCheck.ts
@@ -3,20 +3,12 @@ import { IUserStore } from '@app/store/user'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
-import { useAuth } from './useAuth'
 
 const useOwnershipCheck = () => {
   const navigate = useNavigate()
-  const { isLogin } = useAuth()
   const { userInfo } = useSelector<RootState, IUserStore>((s) => s.user)
 
   const checkOwnership = (ownerId?: string, shouldReplaceRoute?: boolean) => {
-    if (!isLogin) {
-      toast.error('You need to login to access this page.')
-      navigate('/login')
-      return false
-    }
-
     if (ownerId && ownerId !== userInfo.id) {
       toast.error('You do not have permission to access this page.')
       navigate('/', { replace: shouldReplaceRoute })
