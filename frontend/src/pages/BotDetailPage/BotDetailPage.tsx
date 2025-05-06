4bdd88e8d7b01d065c075e056e56632da704fd60
Quan <97832906+xmen02052003lx@users.noreply.github.com>
2025-05-06 20:10:33 +0700

    Mtb 219 logout when view bot error (#154)
    * MTB_212_Responsive_UI (#138)
    
    * MTB-219-error-logout-when-view-bot
    
    * fix: error require login to view bot
    
    * refactor: naming convention and add TODO note
    
    ---------
    
    Co-authored-by: Tran Huynh Nha Uyen <127413543+tranuyn@users.noreply.github.com>

--- frontend/src/pages/BotDetailPage/BotDetailPage.tsx
+++ frontend/src/pages/BotDetailPage/BotDetailPage.tsx
@@ -30,7 +30,7 @@ import { AppStatus } from '@app/enums/AppStatus.enum'
 import Button from '@app/mtb-ui/Button'
 function BotDetailPage() {
   const navigate = useNavigate()
-  const [getMezonAppDetail, { isError, error, isSuccess }] = useLazyMezonAppControllerGetMezonAppDetailQuery()
+  const [getMezonAppDetail, { isError, error, isSuccess, data: getMezonAppDetailApiResponse }] = useLazyMezonAppControllerGetMezonAppDetailQuery()
   const [getrelatedMezonApp] = useLazyMezonAppControllerGetRelatedMezonAppQuery()
   const [getTagList] = useLazyTagControllerGetTagsQuery()
   const [getRatingsByApp, { isLoading: isLoadingReview }] = useLazyRatingControllerGetRatingsByAppQuery()
@@ -81,10 +81,11 @@ function BotDetailPage() {
     }
   }, [isError, error]);
   useEffect(() => {
-      if (mezonAppDetail?.status !== AppStatus.PUBLISHED) {
-        checkOwnership(mezonAppDetail?.owner?.id, true);
-      }
-    }, [mezonAppDetail])
+    // TODO: improve logic
+    if (getMezonAppDetailApiResponse?.data && getMezonAppDetailApiResponse?.data?.status !== AppStatus.PUBLISHED) {
+      checkOwnership(getMezonAppDetailApiResponse.data?.owner?.id, true);
+    }
+  }, [getMezonAppDetailApiResponse])
 
   const onLoadMore = async () => {
     if (botId && botId !== 'undefined' && botId.trim() !== '') {
